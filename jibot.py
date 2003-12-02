1f45fb723b00fc073898b2a0efb0fe5ecc955722
Kevin Marks <kevinmarks@gmail.com>
2003-12-02 11:44:10 +0000

    fix bad indent

--- jibot.py
+++ jibot.py
@@ -16,8 +16,8 @@ __contributors__ = ['Kevin Marks', 'Jens-Christian Fischer', 'Joi Ito']
 __copyright__ = "Copyright (c) 2003 Victor R. Ruiz"
 __license__ = "GPL"
 __version__ = "0.4"
-__cvsversion__ = "$Revision: 1.61 $"[11:-2]
-__date__ = "$Date: 2003/12/02 11:33:28 $"[7:-2]
+__cvsversion__ = "$Revision: 1.62 $"[11:-2]
+__date__ = "$Date: 2003/12/02 11:44:10 $"[7:-2]
 
 import string, sys, os, re
 import random, time, xmlrpclib
@@ -673,8 +673,8 @@ class jibot(irclib.irc):
 		except:
 			self.say('I cannot search %s. Sorry about this. I know it\'s a bit silly.' % (m))
 
-def cmd_isbn(self, m):
-	self.cmd_asin(m)
+	def cmd_isbn(self, m):
+		self.cmd_asin(m)
 	
 	def cmd_learn(self, m):
 		""" Learn a definition """
